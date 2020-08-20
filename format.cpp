4d8e1572f48c632cbb4b6d2df3f164536cc714c3
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2020-08-20 18:25:28 +0200

    Make the duration of longer calls more readable and more l10n-able

--- format.cpp
+++ format.cpp
@@ -15,3 +15,13 @@ std::string formatMessage(const char *fmt, const std::string &s)
 {
     return formatMessage(fmt, {s});
 }
+
+std::string formatDuration(int32_t seconds)
+{
+    int32_t hours = seconds / 3600;
+    seconds -= hours * 3600;
+    int32_t minutes = seconds / 60;
+    seconds -= minutes * 60;
+    // TRANSLATOR: Time format. For HH:MM:SS use "{:02}:{:02}:{:02}" without the quotes.
+    return fmt::format(_("{:02}:{:02}:{:02}"), hours, minutes, seconds);
+}
