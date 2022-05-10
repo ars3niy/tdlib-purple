573ee2166767c20a5c4a5451b5c3a954731f941b
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2022-05-10 19:09:13 +0200

    Implement spoiler tags by advancing td

--- build_and_install.sh
+++ build_and_install.sh
@@ -9,7 +9,7 @@ mkdir build
 pushd build
   git clone https://github.com/tdlib/td.git
   pushd td
-    git checkout v1.8.0
+    git checkout 1d1bc07eded7d3ee7df7c567e008bbf926cba081 # spoilers
     mkdir build
     pushd build
       cmake -DCMAKE_BUILD_TYPE=Release ..
